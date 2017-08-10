43effd7433fe47e549ff11b778d1062516a6afe8
Craig Watson <craig@watsons.ch>
2017-08-10 12:05:15 -0400

    Added analog i/o correction based on calibration data

--- src/things.cpp
+++ src/things.cpp
@@ -121,16 +121,30 @@ void PressureController::setValue(uint8_t value)
         mSetPointValue = value;
         Log.notice("Setting pressure to %d \n", value);
 
+        /*
         int toWrite = value;
         // Rescale the value if necessary
         if (mSetPointMaxValue != UINT8_MAX) 
             int toWrite = double(value)/double(UINT8_MAX) * mSetPointMaxValue;
+            */
 
-        if (mSetPointPin == DAC0 || mSetPointPin == DAC1) 
+        double x = double(value)/double(UINT8_MAX);
+
+        if (mSetPointPin == DAC0 || mSetPointPin == DAC1) {
+            double y = 0.3306*pow(x, 3) - 0.428*pow(x, 2) + 1.0961*x - 0.0258;
+            int toWrite = std::min(mSetPointMaxValue, 
+                                   std::max(0, int(round(y*mSetPointMaxValue))));
+
             dacWrite(mSetPointPin, toWrite);
-        else 
-            ledcWrite(1, toWrite);
+        }
+        else {
 
+            double y = 0.3689*pow(x, 3) - 0.4627*pow(x, 2) + 1.0525*x - 0.004;
+
+            int toWrite = std::min(mSetPointMaxValue, 
+                                   std::max(0, int(round(y*mSetPointMaxValue))));
+            ledcWrite(1, toWrite);
+        }
     }
 }
 
@@ -138,8 +152,22 @@ uint8_t PressureController::getValue()
 {
     int val = analogRead(mMeasurementPin);
 
+    /* The result of analogRead doesn't correspond linearly to the voltage applied;
+       see: https://github.com/espressif/esp-idf/issues/164
+       So a calibration curve was established by applying voltages between 0 and 5v
+       to the pressure controller pins, recording the result of analogRead, and fitting
+       a polynomial to the data.
+       If and when Espressif fix the analogRead() function to take into account the
+       non-linearity, the following fit can be replaced by a simple rescaling
+    */
+
+    double x = val;
+    double y = -3e-12*pow(x, 3) - 7e-10*pow(x, 2) + 0.0003*x + 0.0169;
+
+    mMeasuredValue = std::min(255, std::max(0, int(round(y*UINT8_MAX))));
+
     // Rescale value to match range of uint8_t (0 to 255)
-    mMeasuredValue = double(val)/double(mMeasurementMaxValue) * UINT8_MAX;
+    //mMeasuredValue = double(val)/double(mMeasurementMaxValue) * UINT8_MAX;
 
     return mMeasuredValue;
 }
